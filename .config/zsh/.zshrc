c849d5d95a5ec20e204aef761ef64f8d76fb7b2e
vaultgoblin64 <vaultgoblin64@users.noreply.github.com>
2025-12-28 02:05:56 +0100

    add tmux, update hypr, kitty & zsh

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -17,3 +17,6 @@ source "$ZDOTDIR/prompt.zsh"       # Prompt/theme
 
 # Syntax highlighting must be loaded LAST
 source "$ZDOTDIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -9,6 +9,8 @@ alias ls='lsd'
 alias ll='lsd -la'
 alias la='lsd -A'
 alias l='lsd -CF'
+alias cat='bat'
+alias c='clear'
 
 # neovim accessibility
 alias vim='nvim'
@@ -21,3 +23,7 @@ alias mv='mv -i'
 # Claude Clode
 alias ccc='claude -c --dangerously-skip-permissions'
 alias ccr='claude -r --dangerously-skip-permissions'
+
+# pacman
+alias syu='sudo pacman -Syu'
+alias s='sudo pacman -S'
